6470ac11f38f58229d25296cf34b4d636e8a7590
Wellington Silverio <wellington.alves.silverio@gmail.com>
2019-02-26 00:55:32 -0300

    doc: refactored the places and add a description

--- index.js
+++ index.js
@@ -7,57 +7,69 @@ const driver = new webdriver.Builder()
   .forBrowser('chrome') // http://chromedriver.chromium.org/downloads
   .build()
 
+/**
+ * Função para iniciar a app para cada resultado das tendencias
+ */
 async function start() {
   asyncForEach(await selectNextTrend(), trend => {
     questions(trend)
   })
-}
 
-function questions(searchTerm) {
-  const content = {}
+  /**
+   * Função para definir o prefixo perguntando para o usuário
+   * @param {String} searchTerm - Termo da busca
+   */
+  function questions(searchTerm) {
+    const content = {}
 
-  // content.searchTerm = askAndReturnSearchTerm()
-  content.searchTerm = searchTerm
-  content.prefix = askAndReturnPrefix(searchTerm)
+    // content.searchTerm = askAndReturnSearchTerm()
+    content.searchTerm = searchTerm
+    content.prefix = askAndReturnPrefix(searchTerm)
 
-  function askAndReturnSearchTerm() {
-    return readline.question('Type a Wikipedia search term: ')
+    function askAndReturnSearchTerm() {
+      return readline.question('Type a Wikipedia search term: ')
+    }
+
+    function askAndReturnPrefix(searchTerm) {
+      const prefixes = ['Who is', 'What is', 'The history of']
+      const selectedPrefixIndex = readline.keyInSelect(prefixes, `Choose an option for ${searchTerm}: `)
+      const selectedPrefixText = prefixes[selectedPrefixIndex]
+
+      return selectedPrefixText
+    }
+
+    console.log(content)
   }
 
-  function askAndReturnPrefix(searchTerm) {
-    const prefixes = ['Who is', 'What is', 'The history of']
-    const selectedPrefixIndex = readline.keyInSelect(prefixes, `Choose an option for ${searchTerm}: `)
-    const selectedPrefixText = prefixes[selectedPrefixIndex]
+  /**
+   * Função para buscar uma nova tendencia no google
+   */
+  const selectNextTrend = async () => {
+    let searchTerm = []
 
-    return selectedPrefixText
+    // Pesquisas em Alta
+    await driver.get('https://trends.google.com/trends/trendingsearches/daily?geo=BR')
+    
+    // xpath: //tagname[@attribute='value']
+    const list = await driver.findElement(By.xpath("//md-list[@class='md-list-block']"))
+    const details = await list.findElements(By.xpath("//div[@class='details']/div[@class='details-top']"))
+    
+    await asyncForEach(details, async title => {
+      searchTerm.push(await title.getText())
+    })
+    await driver.close()
+
+    return searchTerm
   }
 
-  console.log(content)
-}
-
-
-const asyncForEach = async (array, callback) => {
-  for (let index = 0; index < array.length; index++) {
-    await callback(array[index], index, array);
+  /**
+   * Função asyncrona para um for dos items dentro do array
+   */
+  async function asyncForEach(array, callback) {
+    for (let index = 0; index < array.length; index++) {
+      await callback(array[index], index, array);
+    }
   }
-}
-
-/**
- * Função para buscar uma nova tendencia no google
- */
-const selectNextTrend = async () => {
-  let searchTerm = []
-
-  // Pesquisas em Alta
-  await driver.get('https://trends.google.com/trends/trendingsearches/daily?geo=BR')
-  // xpath: //tagname[@attribute='value']
-  const list = await driver.findElement(By.xpath("//md-list[@class='md-list-block']"))
-  const details = await list.findElements(By.xpath("//div[@class='details']/div[@class='details-top']"))
-  await asyncForEach(details, async title => {
-    searchTerm.push(await title.getText())
-  })
-  await driver.close()
-  return searchTerm
 }
 
 start()
