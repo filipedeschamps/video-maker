bcc19b8099062168e74e1bac57a17e4fbd87c8dc
Paulo R <proberto.macedo@gmail.com>
2019-02-28 18:00:02 +0200

    Added eslint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "plugins": ["jest"],
+    rules: {
+        'no-console': 'off',
+    },
+    "env": {
+        "jest/globals": true
+    }
+};
