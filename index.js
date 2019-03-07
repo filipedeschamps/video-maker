295aea440fc54fde6422ee9647c0e2d90324ec13
Rodolfo Silva <contato@rodolfosilva.com>
2019-03-07 18:42:48 -0300

    fix: ajusta a indentação

--- index.js
+++ index.js
@@ -6,22 +6,22 @@ const robots = {
 function askAndReturnAnswers() {
   const questions = [
     {
-        type: 'text',
-        name: 'searchTerm',
-        message: 'Type a Wikipedia search term:',
-        validate: value => typeof value === 'string' ? value.trim() !== '' : false,
+      type: 'text',
+      name: 'searchTerm',
+      message: 'Type a Wikipedia search term:',
+      validate: value => typeof value === 'string' ? value.trim() !== '' : false,
     },
     {
-        type: 'select',
-        name: 'prefix',
-        message: 'Choose one option:',
-        choices: ['Who is', 'What is', 'The history of'],
-        validate: value => typeof value === 'string' ? value.trim() !== '' : false,
+      type: 'select',
+      name: 'prefix',
+      message: 'Choose one option:',
+      choices: ['Who is', 'What is', 'The history of'],
+      validate: value => typeof value === 'string' ? value.trim() !== '' : false,
     }
   ]
 
   return new Promise(async (resolve, reject) => {
-    const promptOptions = { 
+    const promptOptions = {
       onCancel: () => reject(new Error('The user has stopped answer'))
     }
     const response = await prompts(questions, promptOptions)
