49b54ecb7bebf312bf94506ba70cb606cc204766
Leo Dutra <leodutra.br@gmail.com>
2019-03-22 01:54:58 -0300

    Move funções para fora de closure, evitando recriação por escopo.

--- robots/text.js
+++ robots/text.js
@@ -22,85 +22,84 @@ async function robot() {
   await fetchKeywordsOfAllSentences(content)
 
   state.save(content)
+}
 
-  async function fetchContentFromWikipedia(content) {
-    const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey)
-    const wikipediaAlgorithm = algorithmiaAuthenticated.algo('web/WikipediaParser/0.1.2')
-    const wikipediaResponse = await wikipediaAlgorithm.pipe(content.searchTerm)
-    const wikipediaContent = wikipediaResponse.get()
+async function fetchContentFromWikipedia(content) {
+  const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey)
+  const wikipediaAlgorithm = algorithmiaAuthenticated.algo('web/WikipediaParser/0.1.2')
+  const wikipediaResponse = await wikipediaAlgorithm.pipe(content.searchTerm)
+  const wikipediaContent = wikipediaResponse.get()
 
-    content.sourceContentOriginal = wikipediaContent.content
-  }
+  content.sourceContentOriginal = wikipediaContent.content
+}
 
-  function sanitizeContent(content) {
-    const withoutBlankLinesAndMarkdown = removeBlankLinesAndMarkdown(content.sourceContentOriginal)
-    const withoutDatesInParentheses = removeDatesInParentheses(withoutBlankLinesAndMarkdown)
+function sanitizeContent(content) {
+  const withoutBlankLinesAndMarkdown = removeBlankLinesAndMarkdown(content.sourceContentOriginal)
+  const withoutDatesInParentheses = removeDatesInParentheses(withoutBlankLinesAndMarkdown)
 
-    content.sourceContentSanitized = withoutDatesInParentheses
+  content.sourceContentSanitized = withoutDatesInParentheses
 
-    function removeBlankLinesAndMarkdown(text) {
-      const allLines = text.split('\n')
+  function removeBlankLinesAndMarkdown(text) {
+    const allLines = text.split('\n')
 
-      const withoutBlankLinesAndMarkdown = allLines.filter((line) => {
-        if (line.trim().length === 0 || line.trim().startsWith('=')) {
-          return false
-        }
+    const withoutBlankLinesAndMarkdown = allLines.filter((line) => {
+      if (line.trim().length === 0 || line.trim().startsWith('=')) {
+        return false
+      }
 
-        return true
-      })
-
-      return withoutBlankLinesAndMarkdown.join(' ')
-    }
-  }
-
-  function removeDatesInParentheses(text) {
-    return text.replace(/\((?:\([^()]*\)|[^()])*\)/gm, '').replace(/  /g,' ')
-  }
-
-  function breakContentIntoSentences(content) {
-    content.sentences = []
-
-    const sentences = sentenceBoundaryDetection.sentences(content.sourceContentSanitized)
-    sentences.forEach((sentence) => {
-      content.sentences.push({
-        text: sentence,
-        keywords: [],
-        images: []
-      })
+      return true
     })
+    
+    return withoutBlankLinesAndMarkdown.join(' ')
   }
+}
 
-  function limitMaximumSentences(content) {
-    content.sentences = content.sentences.slice(0, content.maximumSentences)
-  }
+function removeDatesInParentheses(text) {
+  return text.replace(/\((?:\([^()]*\)|[^()])*\)/gm, '').replace(/  /g,' ')
+}
 
-  async function fetchKeywordsOfAllSentences(content) {
-    for (const sentence of content.sentences) {
-      sentence.keywords = await fetchWatsonAndReturnKeywords(sentence.text)
-    }
-  }
+function breakContentIntoSentences(content) {
+  content.sentences = []
 
-  async function fetchWatsonAndReturnKeywords(sentence) {
-    return new Promise((resolve, reject) => {
-      nlu.analyze({
-        text: sentence,
-        features: {
-          keywords: {}
-        }
-      }, (error, response) => {
-        if (error) {
-          throw error
-        }
-
-        const keywords = response.keywords.map((keyword) => {
-          return keyword.text
-        })
-
-        resolve(keywords)
-      })
+  const sentences = sentenceBoundaryDetection.sentences(content.sourceContentSanitized)
+  sentences.forEach((sentence) => {
+    content.sentences.push({
+      text: sentence,
+      keywords: [],
+      images: []
     })
-  }
+  })
+}
 
+function limitMaximumSentences(content) {
+  content.sentences = content.sentences.slice(0, content.maximumSentences)
+}
+
+async function fetchKeywordsOfAllSentences(content) {
+  for (const sentence of content.sentences) {
+    sentence.keywords = await fetchWatsonAndReturnKeywords(sentence.text)
+  }
+}
+
+async function fetchWatsonAndReturnKeywords(sentence) {
+  return new Promise((resolve, reject) => {
+    nlu.analyze({
+      text: sentence,
+      features: {
+        keywords: {}
+      }
+    }, (error, response) => {
+      if (error) {
+        throw error
+      }
+
+      const keywords = response.keywords.map((keyword) => {
+        return keyword.text
+      })
+
+      resolve(keywords)
+    })
+  })
 }
 
 module.exports = robot
