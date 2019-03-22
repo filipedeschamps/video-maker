9048b97ef716eedfa38b4a60a63ea33493a8a75d
Leo Dutra <leodutra.br@gmail.com>
2019-03-22 01:52:36 -0300

    Agrupa import de state.js com outros imports

--- robots/text.js
+++ robots/text.js
@@ -4,14 +4,13 @@ const sentenceBoundaryDetection = require('sbd')
 
 const watsonApiKey = require('../credentials/watson-nlu.json').apikey
 const NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js')
- 
+const state = require('./state.js')
+
 const nlu = new NaturalLanguageUnderstandingV1({
   iam_apikey: watsonApiKey,
   version: '2018-04-05',
   url: 'https://gateway.watsonplatform.net/natural-language-understanding/api/'
 })
-
-const state = require('./state.js')
 
 async function robot() {
   const content = state.load()
