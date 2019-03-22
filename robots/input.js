e7c9c7e3bd0d51beee851db54949271cbb27a4ae
Leo Dutra <leodutra.br@gmail.com>
2019-03-22 01:48:55 -0300

    Simplifica código e evita que funções sejam recriada a cada chamada de robot()

--- robots/input.js
+++ robots/input.js
@@ -3,25 +3,23 @@ const state = require('./state.js')
 
 function robot() {
   const content = {
-    maximumSentences: 7
+    maximumSentences: 7,
+    searchTerm: askAndReturnSearchTerm(),
+    prefix: askAndReturnPrefix()
   }
-
-  content.searchTerm = askAndReturnSearchTerm()
-  content.prefix = askAndReturnPrefix()
   state.save(content)
+}
 
-  function askAndReturnSearchTerm() {
-    return readline.question('Type a Wikipedia search term: ')
-  }
+function askAndReturnSearchTerm() {
+  return readline.question('Type a Wikipedia search term: ')
+}
 
-  function askAndReturnPrefix() {
-    const prefixes = ['Who is', 'What is', 'The history of']
-    const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose one option: ')
-    const selectedPrefixText = prefixes[selectedPrefixIndex]
+function askAndReturnPrefix() {
+  const prefixes = ['Who is', 'What is', 'The history of']
+  const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose one option: ')
+  const selectedPrefixText = prefixes[selectedPrefixIndex]
 
-    return selectedPrefixText
-  }
-
+  return selectedPrefixText
 }
 
 module.exports = robot
