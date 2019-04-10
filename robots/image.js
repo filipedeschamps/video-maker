7af49db006dfc116d7584c067fc6926b563460af
Felipe Marchi <fel.marchi@gmail.com>
2019-04-10 16:58:09 -0300

    Correção sintaxe

--- robots/image.js
+++ robots/image.js
@@ -65,7 +65,7 @@ async function robot() {
 
   async function downloadAndSave(url, fileName) {
     return imageDownloader.image({
-      url, url,
+      url: url,
       dest: `./content/${fileName}`
     })
   }
