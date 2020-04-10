2ad5769de7abc93655761e87903a66d7d2bfda1e
Paulo Jemile <paulo.jemile@gmail.com>
2020-04-10 20:17:52 -0300

    removing unnecessary require of state

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const robots = {
   input: require('./robots/input.js'),
   text: require('./robots/text.js'),
-  state: require('./robots/state.js'),
   image: require('./robots/image.js'),
   video: require('./robots/video.js'),
   youtube: require('./robots/youtube.js')
