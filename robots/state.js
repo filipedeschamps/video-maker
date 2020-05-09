0e72a2832a0a282e12ca18ac6602c843dd59c83c
roma22a <romaninicontato@gmail.com>
2020-05-09 18:06:14 -0400

    feat: render video with FFmpeg
    render with FFmpeg if it is a linux system or After Effects is not found

--- robots/state.js
+++ robots/state.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 const contentFilePath = './content.json'
-const scriptFilePath = './content/after-effects-script.js'
+const scriptFilePath = './content/video-script.js'
 
 function save(content) {
   const contentString = JSON.stringify(content)
--- robots/video.js
+++ robots/video.js
@@ -2,8 +2,21 @@ const gm = require('gm').subClass({imageMagick: true})
 const state = require('./state.js')
 const spawn = require('child_process').spawn
 const path = require('path')
-const os = require('os');
 const rootPath = path.resolve(__dirname, '..')
+
+const os = require('os')
+const fs = require('fs')
+
+const videoshow = require('videoshow')
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path
+const ffprobePath = require('@ffprobe-installer/ffprobe').path
+
+const audio = path.join(__dirname, '../templates/1/newsroom.mp3')
+const video = path.join(__dirname, '../content/output.mp4')
+
+let ffmpeg = require('fluent-ffmpeg')
+ffmpeg.setFfmpegPath(ffmpegPath)
+ffmpeg.setFfprobePath(ffprobePath)
 
 const fromRoot = relPath => path.resolve(rootPath, relPath)
 
@@ -14,8 +27,8 @@ async function robot() {
   await convertAllImages(content)
   await createAllSentenceImages(content)
   await createYouTubeThumbnail()
-  await createAfterEffectsScript(content)
-  await renderVideoWithAfterEffects()
+  await createVideoScript(content)
+  await renderVideo(content)
 
   state.save(content)
 
@@ -139,22 +152,33 @@ async function robot() {
     })
   }
 
-  async function createAfterEffectsScript(content) {
+  async function createVideoScript(content) {
     await state.saveScript(content)
   }
 
-  async function renderVideoWithAfterEffects() {
-    return new Promise((resolve, reject) => {
-      const systemPlatform=os.platform
-      
-      if (systemPlatform== 'darwin'){
-        const aerenderFilePath = '/Applications/Adobe After Effects CC 2019/aerender'
-      }else if (systemPlatform=='win32'){
-        const aerenderFilePath = '%programfiles%\Adobe\Adobe After Effects CC\Arquivos de suporte\aerender.exe'
-      }else{
-        return reject(new Error('System not Supported'))
+  async function renderVideo(content) {
+    let aerenderFilePath;
+
+    const systemPlatform = os.platform
+    if (systemPlatform == 'darwin'){
+      aerenderFilePath = '/Applications/Adobe After Effects CC 2019/aerender'
+    }else if (systemPlatform =='win32'){
+      aerenderFilePath = '%programfiles%\Adobe\Adobe After Effects CC\Arquivos de suporte\aerender.exe'
+    }
+
+    try {
+      if (fs.existsSync(aerenderFilePath)) {
+        return await renderVideoWithAfterEffects(aerenderFilePath)
       }
-      
+    } catch(err) {
+      console.error(err)
+    }
+
+    return await renderVideoWithFFmpeg(content)
+  }
+
+  async function renderVideoWithAfterEffects(aerenderFilePath) {
+    return new Promise((resolve, reject) => {
       const templateFilePath = fromRoot('./templates/1/template.aep')
       const destinationFilePath = fromRoot('./content/output.mov')
 
@@ -174,6 +198,71 @@ async function robot() {
         console.log('> [video-robot] After Effects closed')
         resolve()
       })
+    })
+  }
+
+  async function renderVideoWithFFmpeg(content) {
+    return new Promise((resolve, reject) => {
+      let images = []
+
+      for (
+        let sentenceIndex = 0;
+        sentenceIndex < content.sentences.length;
+        sentenceIndex++
+      ) {
+        images.push({
+          path: `./content/${sentenceIndex}-converted.png`,
+          caption: content.sentences[sentenceIndex].text
+        })
+      }
+
+      const videoOptions = {
+        fps: 25,
+        loop: 5, // seconds
+        transition: true,
+        transitionDuration: 1, // seconds
+        videoBitrate: 1024,
+        videoCodec: "libx264",
+        size: "640x?",
+        audioBitrate: "128k",
+        audioChannels: 2,
+        format: "mp4",
+        pixelFormat: "yuv420p",
+        useSubRipSubtitles: false, // Use ASS/SSA subtitles instead
+        subtitleStyle: {
+          Fontname: "Verdana",
+          Fontsize: "26",
+          PrimaryColour: "11861244",
+          SecondaryColour: "11861244",
+          TertiaryColour: "11861244",
+          BackColour: "-2147483640",
+          Bold: "2",
+          Italic: "0",
+          BorderStyle: "2",
+          Outline: "2",
+          Shadow: "3",
+          Alignment: "1", // left, middle, right
+          MarginL: "40",
+          MarginR: "60",
+          MarginV: "40"
+        }
+      }
+
+      videoshow(images, videoOptions)
+        .audio(audio)
+        .save(video)
+        .on("start", () => {
+          console.log('> [video-robot] Starting FFmpeg')
+        })
+        .on("error", (err, stdout, stderr) => {
+          console.error("Error:", err)
+          console.error("ffmpeg stderr:", stderr)
+          reject(err)
+        })
+        .on("end", () => {
+          console.log('> [video-robot] FFmpeg closed')
+          resolve()
+        })
     })
   }
 
